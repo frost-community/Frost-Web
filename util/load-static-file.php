fa3cec1892e638ae728fb0498e6d2e27fa64e409
Marihachi <marihachi0620@gmail.com>
2015-12-22 21:09:49 +0900

    remove comment

--- util/load-static-file.php
+++ util/load-static-file.php
@@ -2,30 +2,27 @@
 
 function LoadStaticFile ($res, $filePath) {
 	global $app;
+
 	require_once('content-types.php');
 
-	//if(strpos($fileName, "..") === false) {
-	//	$app->notFound();
-	//} else {
-		$content = null;
-		$extension = pathinfo($filePath)['extension'];
-		$contentType = null;
+	$content = null;
+	$extension = pathinfo($filePath)['extension'];
+	$contentType = null;
 
-		foreach ($ContentTypes as $exte => $type) {
-			if ($extension === $exte) {
-				$contentType = $type;
-				break;
-			}
+	foreach ($ContentTypes as $exte => $type) {
+		if ($extension === $exte) {
+			$contentType = $type;
+			break;
 		}
+	}
 
-		if ($contentType !== null)
-			$res->headers->set('Content-Type', $contentType);
+	if ($contentType !== null)
+		$res->headers->set('Content-Type', $contentType);
 
-		if (file_exists($filePath) && !!($temp = file_get_contents($filePath)))
-			$content = $temp;
-		else
-			$app->notFound();
+	if (file_exists($filePath) && !!($temp = file_get_contents($filePath)))
+		$content = $temp;
+	else
+		$app->notFound();
 
-		$res->setBody($content);
-	//}
+	$res->setBody($content);
 }

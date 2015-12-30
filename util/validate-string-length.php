7d1ccccf65ddaacf711e1e0587210c9df6c82d74
Marihachi <marihachi0620@gmail.com>
2015-12-30 21:23:41 +0900

    怒りのコミット

--- util/validate-string-length.php
+++ util/validate-string-length.php
@@ -1,13 +1,9 @@
 <?php
 
 function validateStringLength ($target, $minLength = null, $maxLength = null) {
-	$flag = true;
-
 	if ($minLength !== null)
-		$flag = strlen($target) >= $minLength;
-
+		return strlen($target) >= $minLength;
 	if ($maxLength !== null)
-		$flag = strlen($target) <= $maxLength;
-
-	return $flag;
+		return strlen($target) <= $maxLength;
+	return true;
 }

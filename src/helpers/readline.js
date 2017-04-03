1ec3828ffd916cdfaa359ed96b0a581daa8957fe
Marihachi <marihachi0620@gmail.com>
2017-04-04 04:39:48 +0900

    update

---
+++ src/helpers/readline.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = (message) => new Promise((resolve) => {
+	const rl = require('readline').createInterface(process.stdin, process.stdout);
+	rl.question(message, (ans) => {
+		resolve(ans);
+		rl.close();
+	});
+});

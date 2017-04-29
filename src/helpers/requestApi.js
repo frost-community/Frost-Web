573db7cedd52374252ff8f328ad126178e9acf4d
Marihachi <marihachi0620@gmail.com>
2017-04-29 18:10:01 +0900

    rename

--- src/helpers/requestApi.js
+++ src/helpers/requestApi.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const path = require('path');
 const config = require('./loadConfig')();
 const request = require('./requestAsync');
 
 module.exports = async (method, endpoint, body, headers) => {
 	let requestHeaders = {'Content-Type': 'application/json'};
 	requestHeaders = Object.assign(requestHeaders, headers == null ? {} : headers);
-	return await request(`https://${config.web.apiHost}${endpoint}`, {
+	return await request(`${config.web.apiBaseUrl}${endpoint}`, {
 		method: method,
 		json: true,
 		headers: requestHeaders,

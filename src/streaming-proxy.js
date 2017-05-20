1769736144b630ba5f0814af5d18f0e2cb930e5b
Marihachi <marihachi0620@gmail.com>
2017-05-20 17:28:19 +0900

    update

--- src/streaming-proxy.js
+++ src/streaming-proxy.js
@@ -14,8 +14,9 @@ module.exports = (http, sessionStore, config) => {
 			const frontManager = new (require('./helpers/server-streaming-manager'))(ioServerToFront, ioServerToFrontSocket, {});
 			const session = await getSessionFromCookieAsync(ioServerToFrontSocket.request.headers.cookie, sessionCookieName, config.web.session.SecretToken, sessionStore);
 
-			if (!session.accessKey) {
+			if (session.accessKey == null) {
 				frontManager.stream('error', {message: 'unauthorized'});
+				frontManager.disconnect();
 				return;
 			}
 

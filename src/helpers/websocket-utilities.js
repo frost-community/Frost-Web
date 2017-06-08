07388e7cef92c78cedaff657b8b1d83044ca738f
Marihachi <marihachi0620@gmail.com>
2017-06-09 02:42:03 +0900

    add utility

---
+++ src/helpers/websocket-utilities.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const WebSocket = require('websocket');
+const events = require('websocket-events');
+
+class WebSocketClientUtility {
+	/**
+	 * 非同期的に接続してコネクションを取得します。
+	 */
+	static connectAsync(requestUrl, protocols, origin, headers) {
+		return new Promise((resolve, reject) => {
+			const client = new WebSocket.client();
+			client.on('connect', (connection) => {
+				resolve(connection);
+			});
+			client.on('connectFailed', (err) => {
+				reject(err);
+			});
+			client.connect(requestUrl, protocols, origin, headers);
+		});
+	}
+}
+exports.WebSocketClientUtility = WebSocketClientUtility;
+
+class WebSocketConnectionUtility {
+	static addExtensionMethods(connection) {
+		events(connection);
+
+		/**
+		 * 指定されたイベントが最初に受信されるまで待機します
+		 */
+		connection.onceAsync = (eventName) => new Promise((resolve) => {
+			connection.once(eventName, (data) => {
+				resolve(data);
+			});
+		});
+	}
+
+	static createEventName(prefix, type) {
+		return `${prefix}:${type}`;
+	}
+}
+exports.WebSocketConnectionUtility = WebSocketConnectionUtility;

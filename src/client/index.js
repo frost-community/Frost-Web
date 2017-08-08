5141db75fc9f9860a85907ae5c3714432ac5d8f3
Marihachi <marihachi0620@gmail.com>
2017-08-09 03:49:24 +0900

    [wip]implement auth (reverted from commit 1f0079f4cbf7fde01ada32e494f8d295cdb88ab4)

--- src/client/index.js
+++ src/client/index.js
@@ -21,8 +21,6 @@ require('./tags/frost-follow-button.tag');
 // dev
 require('./tags/frost-applications.tag');
 require('./tags/frost-create-application-form.tag');
-// authorize
-require('./tags/frost-appauth-form.tag');
 
 const mixinGlobal = {};
 

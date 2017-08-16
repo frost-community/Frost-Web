758b4a165137b048ba4508cf5d57bdda746e1554
Marihachi <marihachi0620@gmail.com>
2017-08-16 21:51:57 +0900

    add api request class that use streaming 'rest'

---
+++ src/client/helpers/StreamingRest.js
@@ -0,0 +1,39 @@
+class StreamingRest {
+	constructor(webSocketConnection) {
+		this.connection = webSocketConnection;
+	}
+
+	requestAsync(method, endpoint, requestContent, apiVersion) {
+		return new Promise((resolve, reject) => {
+			requestContent = requestContent || {};
+			apiVersion = apiVersion || 1;
+
+			const handler = rest => {
+				if (rest.request.method == method && rest.request.endpoint == endpoint) {
+					if (rest.success) {
+						if (rest.response.user != null) {
+							resolve(rest);
+						}
+						reject(new Error(`api error: failed to fetch user data. ${rest.response.message}`));
+					}
+					reject(new Error(`internal error: failed to fetch user data. ${rest.message}`));
+
+					this.connection.off('rest', handler);
+				}
+			};
+			this.connection.on('rest', handler);
+
+			let request = {
+				method: method,
+				endpoint: endpoint,
+				headers: {
+					'x-api-version': apiVersion
+				}
+			};
+			request = Object.assign(request, requestContent);
+			this.connection.sendEvent('rest', {request: request});
+		});
+	}
+}
+
+module.exports = StreamingRest;

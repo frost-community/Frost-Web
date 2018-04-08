f33ea25a6af67e221644e40794e50c8734737dd7
marihachi <marihachi0620@gmail.com>
2018-04-09 04:31:43 +0900

    update

--- src/server/helpers/get-session-from-cookie.js
+++ src/server/helpers/get-session-from-cookie.js
@@ -1,16 +1,20 @@
+const { promisify } = require('util');
 const cookie = require('cookie');
 const cookieParser = require('cookie-parser');
-const prominence = require('prominence');
 
 /**
  * Cookieからセッション情報を取得します。
+ * @param {a} store
  */
 module.exports = async (cookieString, sessionCookieName, cookieSecret, store) => {
-	if (cookieString == null)
+	if (cookieString == null) {
 		return null;
+	}
 
-	let cookies = cookieParser.signedCookies(cookie.parse(cookieString), cookieSecret);
-	const session = await prominence(store).get(cookies[sessionCookieName]);
+	const getSession = promisify(store.get);
+
+	const cookies = cookieParser.signedCookies(cookie.parse(cookieString), cookieSecret);
+	const session = await getSession(cookies[sessionCookieName]);
 
 	return session;
 };
